16621c01a1be68ce7443f3a48fa263031a83c575
vastep <47853508+vastep@users.noreply.github.com>
2021-03-23 16:12:17 +0200

    Add Init script

---
+++ init.sh
@@ -0,0 +1,6 @@
+sudo apt update -y
+sudo apt install gcc make perl curl
+
+# PgAdmin
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
