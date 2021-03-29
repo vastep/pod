f433ce391fc5f7ae674fcd4715d49a8c7393f59a
vastep <47853508+vastep@users.noreply.github.com>
2021-03-29 15:15:23 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,3 +6,4 @@ sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 sudo apt install -y pgadmin4-desktop
 
+sudo snap install --classic code
