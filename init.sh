1aee55d4f5d8f2c31aa35199e811277c5e3deb87
vastep <47853508+vastep@users.noreply.github.com>
2021-04-04 16:18:07 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,3 +7,14 @@ sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_
 sudo apt install -y pgadmin4-web
 
 sudo snap install --classic code
+
+# AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+terraform -install-autocomplete
