fcaf243b3f289108a9a7cee9ff1cc76c822290fe
vastep <47853508+vastep@users.noreply.github.com>
2021-12-11 15:05:57 +0200

    Limit arch

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo apt install -y gcc make perl curl
 
 # PgAdmin
 sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
-sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo sh -c 'echo "deb [arch=amd64] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 sudo apt install -y pgadmin4-web
 
 sudo snap install --classic code
